140522e17f89a87c5c3a96009ac8d8195be2758c
Prog84 <bvv198424@rambler.ru>
2021-02-16 15:19:12 +1000

    Rename game to gameover

--- Assets/Group1/Scripts/GameOver.cs
+++ Assets/Group1/Scripts/GameOver.cs
@@ -2,13 +2,12 @@
 using System.Linq;
 using UnityEngine;
 
-public class Game : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameObject _gameOverScreen;
-    [SerializeField] private Player _player;
     [SerializeField] private List<GameObject> _collisionObjects;
 
-    private int _countEnimies = 0;
+    private int _countEnemies = 0;
 
     private void OnEnable()
     {
@@ -17,7 +16,7 @@ public class Game : MonoBehaviour
             if (collisionObject.TryGetComponent(out Enemy enemy))
             {
                 enemy.Dead += OnDead;
-                _countEnimies++;
+                _countEnemies++;
             }
         }
     }
@@ -25,10 +24,10 @@ public class Game : MonoBehaviour
     private void OnDead(Enemy enemy)
     {
         enemy.Dead -= OnDead;
-        _countEnimies--;
+        _countEnemies--;
         _collisionObjects.Remove(enemy.gameObject);
 
-        if (_countEnimies == 0)
+        if (_countEnemies == 0)
         {
             _gameOverScreen.SetActive(true);
         }
